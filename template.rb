7390a4db663392aa7c04e55c490fafa8f54d9492
Jonathan Dean <jon@jonathandean.com>
2022-10-16 11:46:33 -0400

    Ruby version, Ruby gemset, and rspec-rails

---
+++ template.rb
@@ -0,0 +1,19 @@
+
+ruby_version = ask("Which ruby version are you using? This will add it to the .ruby-version file:")
+run "echo \"#{ruby_version}\" > .ruby-version"
+ruby_gemset = ask("Enter a gemset name for .ruby-gemset or just hit enter to skip creation of this file:")
+if ruby_gemset
+  run "echo \"#{ruby_gemset}\" > .ruby-gemset"
+end
+
+gem_group :development, :test do
+  gem "rspec-rails"
+end
+
+after_bundle do
+  generate "rspec:install"
+
+  git :init
+  git add: '.'
+  git commit: "-a -m 'Initial commit'"
+end
