4d1d6626b7454b0b6fc1dfe1f93ec7866250705d
Jonathan Dean <jon@jonathandean.com>
2022-10-16 15:20:17 -0400

    Add comments to existing template items so far

--- template.rb
+++ template.rb
@@ -7,11 +7,15 @@ if ruby_gemset
 end
 
 gem_group :development, :test do
+  # Ease of setting environment variables locally
   gem "dotenv-rails"
+  # rspec for unit tests
   gem "rspec-rails"
+  # Factories over fixtures for tests
   gem "factory_bot_rails"
 end
 
+# Do not commit local env var files to version control as they may have sensitive credentials or dev-only config
 append_to_file ".gitignore", <<-EOS
 
 # Local-only environment variables
@@ -19,16 +23,20 @@ append_to_file ".gitignore", <<-EOS
 .env.*
 EOS
 
+# View components for portions of views with more complex logic
 gem "view_component"
+# Reduce Request logging to a single line in production
 gem "lograge"
 
 # JSON performance
 gem 'multi_json'
 gem 'oj'
 
+# Enable lograte in the staging environment
 environment 'config.lograge.enabled = true', env: 'production'
 
 after_bundle do
+  # Setup rspec
   generate "rspec:install"
 
   git :init
