7cd2d51df1db95a962d0e2ce3700b7a118aa5abb
Jonathan Dean <jon@jonathandean.com>
2022-10-18 22:38:32 -0400

    add bin/ci runner

--- template.rb
+++ template.rb
@@ -108,6 +108,73 @@ end
 App::Cli.start(ARGV)
 EOS
 
+create_file "bin/ci", <<-'EOS'
+#!/usr/bin/env ruby
+
+# Usage: bin/ci [options]
+#
+# --no-[STEP]  Exclude the specified step
+# --only STEP  Run only the specified step
+#
+# Examples:
+#
+#   bin/ci --no-brakeman
+#   bin/ci --only rspec
+
+require "open3"
+require "optparse"
+
+# Define steps.
+# NOTE: The order here determines the order they are performed.
+STEPS = {
+  "bundle-audit" => "bundle audit check --update",
+  "brakeman" => "bundle exec brakeman",
+  "rspec" => "bundle exec rspec"
+}
+
+def perform_step(name, cmd)
+  Open3.popen3(cmd) do |stdin, stdout, stderr, thread|
+    { STDOUT => stdout, STDERR => stderr }.each do |output, input|
+      Thread.new do
+        last_char = nil
+        while char = input.getc do
+          if last_char.nil? || last_char == "\n"
+            output.print "[#{name}] "
+          end
+          output.print char
+          last_char = char
+        end
+      end
+    end
+
+    thread.join
+
+    status = thread.value
+    unless status.success?
+      exit status.exitstatus
+    end
+  end
+end
+
+options = { steps: STEPS.keys }
+
+OptionParser.new do |parser|
+  parser.on("--only=ONLY") do |only|
+    options[:steps] = only.split(",")
+  end
+
+  STEPS.keys.each do |step|
+    parser.on("--no-#{step}") do
+      options[:steps].delete(step)
+    end
+  end
+end.parse!
+
+options[:steps].each do |step|
+  perform_step step, STEPS[step]
+end
+EOS
+
 prepend_to_file "config/routes.rb", <<-EOS
 require 'sidekiq/web'
 EOS
