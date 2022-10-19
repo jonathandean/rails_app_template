33387e6b058cff03d97f15aa1902d37c495f3424
Jonathan Dean <jon@jonathandean.com>
2022-10-18 21:46:00 -0400

    Annotate models and routes

--- template.rb
+++ template.rb
@@ -6,6 +6,11 @@ if ruby_gemset
   run "echo \"#{ruby_gemset}\" > .ruby-gemset"
 end
 
+gem_group :development do
+  # Auto-annotate files with schema and other info
+  gem "annotate"
+end
+
 gem_group :development, :test do
   # Ease of setting environment variables locally
   gem "dotenv-rails"
@@ -14,14 +19,6 @@ gem_group :development, :test do
   # Factories over fixtures for tests
   gem "factory_bot_rails"
 end
-
-# Do not commit local env var files to version control as they may have sensitive credentials or dev-only config
-append_to_file ".gitignore", <<-EOS
-
-# Local-only environment variables
-.env
-.env.*
-EOS
 
 # View components for portions of views with more complex logic
 gem "view_component"
@@ -32,12 +29,25 @@ gem "lograge"
 gem 'multi_json'
 gem 'oj'
 
+# Do not commit local env var files to version control as they may have sensitive credentials or dev-only config
+append_to_file ".gitignore", <<-EOS
+
+# Local-only environment variables
+.env
+.env.*
+EOS
+
 # Enable lograte in the staging environment
 environment 'config.lograge.enabled = true', env: 'production'
 
 after_bundle do
   # Setup rspec
   generate "rspec:install"
+  # Setup annotate
+  generate "annotate:install"
+  # Enable route and model annotation
+  gsub_file "lib/tasks/auto_annotate_models.rake", /'models'(\s*)=>(\s*)'false'/, "'models'                      => 'true'"
+  gsub_file "lib/tasks/auto_annotate_models.rake", /'routes'(\s*)=>(\s*)'false'/, "'routes'                      => 'true'"
 
   git :init
   git add: '.'
